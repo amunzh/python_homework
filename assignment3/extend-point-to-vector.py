563ef0236f26967a77ef4e032d90060cbbae8cf8
amunzh <unzhakova.a.m@gmail.com>
2025-12-09 13:47:07 -0500

    assignment3

---
+++ assignment3/extend-point-to-vector.py
@@ -0,0 +1,36 @@
+import math
+class Point:
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+    
+    def equality(self, other):
+        return self.x == other.x and self.y == other.y
+    def  __str__(self):
+        return f'({self.x}, {self.y})'
+    def euc_dist(self, other):
+        return math.sqrt((other.x-self.x)**2 + (other.y-self.y)**2)
+    
+class Vector(Point):
+    def __init__(self,x,y):
+        super().__init__(x, y)
+    def __str__(self):
+        return f'⟨{self.x}, {self.y}⟩'
+    def __add__(self, other):
+        return Vector(self.x+other.x, self.y+other.y)
+    
+point1 = Point(2,9)
+point2 = Point(-6,10)
+vector1 = Vector(3,5)
+vector2 = Vector(0,-6)
+
+print(point1)
+print(point2)
+print(point1.equality(point2))
+print(point1.equality(point1))
+print(point1.euc_dist(point2))
+
+print(vector1)
+print(vector1+vector2)
+
+    
---
+++ assignment3/hangman-closure.py
@@ -0,0 +1,27 @@
+def make_hangman(secret_word):
+    guesses = []
+    def hangman_closure(gues_let):
+        guesses.append(gues_let)
+        final_hangman =''
+        for letter in secret_word:
+            if letter in guesses:
+                final_hangman += letter
+            else:
+                final_hangman += '_'   
+        print(final_hangman)
+        if secret_word == final_hangman:
+            return True
+        else:
+            return False
+    return hangman_closure
+
+
+secret_wrd = input("Enter secret word:")
+
+x=make_hangman(secret_wrd)
+correct = False
+while not correct:
+    guess = input("Enter your guess:")
+    correct = x(guess)
+if correct:
+    print("Correct!")
